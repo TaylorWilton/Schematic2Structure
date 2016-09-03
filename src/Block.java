88593bc34054a5cb396e4f5f9904c32c5e6072d5
tdeakin <amles.gamer@gmail.com>
2016-09-03 22:35:10 +1200

    Refactored block class to have better getter/setter names

--- src/Block.java
+++ src/Block.java
@@ -5,54 +5,57 @@ public class Block {
     private int _id;
     private String _properties;
     private String _key;
-    private int _x;
-    private int _y;
-    private int _z;
     private String _blockProperties;
     private byte _data;
     private String _name;
 
     /**
      * Block Constructor, creates individual block
-     * @param _id - block id associated with this block
+     *
+     * @param _id   - block id associated with this block
      * @param _name - name of the block
      * @param _data - any block data
-     * @param _y - ypos
-     * @param _z - zpos
-     * @param _x xpos
      */
-    public Block(int _id, String _name, byte _data, int _y, int _z, int _x) {
+    public Block(int _id, String _name, byte _data) {
         this._id = _id;
         this._name = _name;
-        this._data = _data;
-        this._y = _y;
-        this._z = _z;
-        this._x = _x;
 
         // set the key of the object
         this._key = _id + ":" + _data;
 
     }
 
-    public byte get_data() {
+    /**
+     * generate a hashcode for each type of block, used for the palette
+     *
+     * the hashCode is unique to each different type of block, rather than to each unique block.
+     * It is used for checking if a block is already in the palette or not.
+     *
+     * @return - the hashcode corresponding to this type of block;
+     */
+    @Override
+    public int hashCode() {
+
+        return ((_id * 10) + _data) * 10;
+    }
+
+    public byte getData() {
         return _data;
     }
 
-    public String get_properties() {
+    public String getProperties() {
         return _properties;
     }
 
-    public String get_key() {
+    public String getKey() {
         return _key;
     }
 
-    public String get_name() {
+    public String getName() {
         return _name;
     }
 
-
-
-    public void set_properties(String _properties) {
+    public void setProperties(String _properties) {
         this._properties = _properties;
     }
 }
